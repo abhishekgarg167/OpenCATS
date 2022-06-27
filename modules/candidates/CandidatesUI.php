2a21c333050c2ed38e7bbec8cc839eb134871c8a
abhishekgarg167 <87353861+abhishekgarg167@users.noreply.github.com>
2022-06-27 10:31:21 +0530

    Changes for enabling recruiter role

--- modules/candidates/CandidatesUI.php
+++ modules/candidates/CandidatesUI.php
@@ -72,8 +72,7 @@ class CandidatesUI extends UserInterface
         $this->_moduleTabText = 'Candidates';
         $this->_subTabs = array(
             'Add Candidate'     => CATSUtility::getIndexName() . '?m=candidates&amp;a=add*al=' . ACCESS_LEVEL_EDIT . '@candidates.add',
-            'Search Candidates' => CATSUtility::getIndexName() . '?m=candidates&amp;a=search',
-            'Import' => CATSUtility::getIndexName(). '?m=candidates&amp;a=search'
+            'Search Candidates' => CATSUtility::getIndexName() . '?m=candidates&amp;a=search'
         );
     }
 
